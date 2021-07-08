e5b4e1549400f0ac02e29422824e9b8e06846d6e
Nikita213-hub <70839622+Nikita213-hub@users.noreply.github.com>
2021-07-08 12:34:03 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,35 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  let openPriority = ['(', '[', '{', '<'];
+  let closePriority = [')', ']', '}', '>'];
+  let stack = [];
+  let res;
+  for (let bracket of expr) {
+      if (openPreority.includes(bracket)) {
+          stack.push(bracket);
+      }
+  };
+  let errBracket;
+  for (let bracket of expr) {
+      if (closePriority.includes(bracket) && stack < 1) {
+          errBracket = true;
+      } 
+      if (closePriority.includes(bracket) && stack !== 0) {
+          stack.pop()
+      } 
+      
+  };
+  res = stack.length === 0 ? true : false;
+  if (!res || errBracket) {
+      throw new 'ExpressionError: Brackets must be paired';
+  };
+  let divisionByZero = '/ 0';
+  if (expr.includes(divisionByZero)) {
+      throw new 'TypeError: Division by zero.'
+  } else {
+  return (new Function('return ' + expr))()
+  }
 }
 
 module.exports = {
